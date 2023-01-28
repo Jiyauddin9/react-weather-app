724278e7d53155d5ff382aa5cff301e2b057f649
Md. Jiyauddin <98481138+Jiyauddin9@users.noreply.github.com>
2023-01-29 00:35:50 +0530

    first commit

---
+++ src/modules/WeatherInfoComponent.js
@@ -0,0 +1,133 @@
+import React from "react";
+import styled from "styled-components";
+import { WeatherIcons } from "../App";
+
+export const WeatherInfoIcons = {
+    sunset: "/react-weather-app/icon/temp.svg",
+    sunrise: "/react-weather-app/icon/temp.svg",
+    humidity: "/react-weather-app/icon/humidity.svg",
+    wind: "/react-weather-app/icon/wind.svg",
+    pressure: "/react-weather-app/icon/pressure.svg",
+};
+
+const Location = styled.span`
+   margin: 15px auto;
+   text-transform: capitalize;
+   font-size: 28px;
+   font-weight: bold;
+
+`;
+
+const Condition = styled.span`
+margin: 20px auto;
+text-transformation: capitalize;
+font-size: 14px;
+& span{
+    font-size: 28px;
+}
+`;
+
+
+const WeatherInfoLabel = styled.span`
+margin: 20px 25px 10px;
+text-transform: capitalize;
+text-align: start;
+width: 90%;
+font-weight: bold;
+font-size: 14px;
+`;
+
+const WeatherLogo = styled.img`
+  width: 100px;
+  height: 100px;
+  margin: 5px auto;
+`;
+
+const WeatherCondition = styled.div`
+  display: flex;
+  width: 100%;
+  margin: 30px auto;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const WeatherInfoContainer = styled.div`
+display: flex;
+width: 90%;
+flex-direction: row;
+justify-content: space-evenly;
+align-items: center;
+flex-wrap: wrap;
+`;
+
+const InfoContainer = styled.div`
+display: flex;
+margin: 5px 10px;
+flex-direction: row;
+justify-content: space-evenly;
+align-items: center;
+`;
+
+const InfoIcon = styled.img`
+width: 55px;
+height: 38px;
+`;
+
+const InfoLabel = styled.span`
+display: flex;
+flex-direction: column;
+font-size; 15px;
+margin; 15px;
+
+& span{
+    font-size: 15px;
+    text-transform: capitalize;
+}
+`;
+
+const WeatherInfoComponent =(props)=>{
+    const{ name, value } = props;
+    return(
+        <InfoContainer>
+            <InfoIcon src={WeatherInfoIcons[name]} />
+            <InfoLabel>
+                {value}
+                <span>{name}</span>
+            </InfoLabel>
+        </InfoContainer>
+    );
+};
+
+const WeatherComponent = (props) => {
+    const {weather}=props;
+    const isDay = weather?.weather[0].icon?.includes('d');
+    const getTime = (timeStamp) => {
+        return `${new Date(timeStamp * 1000).getHours()} : ${new Date(timeStamp * 1000).getMinutes()}`
+    };
+  return (
+    <>
+      <WeatherCondition>
+        <Condition>
+            <span>{`${Math.floor(weather?.main?.temp - 273)}°C`}</span>
+            {`  |  ${weather?.weather[0].description}`}
+            </Condition>
+        <WeatherLogo src={WeatherIcons[weather?.weather[0].icon]} />
+      </WeatherCondition>
+
+      <Location>{`${weather?.name}, ${weather?.sys?.country}`}</Location>
+
+      <WeatherInfoLabel>Weather Info</WeatherInfoLabel>
+      <WeatherInfoContainer>
+        <WeatherInfoComponent
+         name={isDay ? "sunset" : "sunrise"}
+        value={`${getTime(weather?.sys[isDay ? "sunset" : "sunrise"])}`}
+         />
+        <WeatherInfoComponent name="humidity" value={weather?.main?.humidity} />
+        <WeatherInfoComponent name="wind" value={weather?.wind?.speed} />
+        <WeatherInfoComponent name="pressure" value={weather?.main?.pressure} />
+      </WeatherInfoContainer>
+    </>
+  );
+};
+export default WeatherComponent;
